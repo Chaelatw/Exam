c20dc4ededbe84f438e1060847beb9cbe12f9fc0
Chaela <chaelawills55@gmail.com>
2024-09-12 13:01:24 -0400

    Exam

---
+++ src/main/java/exam1.java
@@ -0,0 +1,32 @@
+
+public class exam1 {
+
+    public static String printFullName(String firstName, String lastName) {
+        String fullName = firstName + lastName;
+        return fullName;
+    }
+
+
+
+    public static void main(String[] args) {
+        String firstName = "Chaela";
+        String lastName = "Wills";
+        char[] sizes = {'S', 'M', 'L'};
+        final String expirationDate = "09/20/2024";
+        System.out.println(printFullName(firstName, lastName));
+    }
+}
+
+    public static int getPrice(int prices) {
+        int price = 5;
+        while (price < 16);
+        int randomNum = (int) (Math.random() * 15);
+        return price;
+
+}
+    public static char printSizes(char sizes){
+    return sizes (sizes.length - 1);
+
+    }
+    import java.util.Scanner;
+    
